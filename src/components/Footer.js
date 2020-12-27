008eb88f4b977021fa87706793ab47bb038de7c3
Shelby-Jean <shelby.gottschall@gmail.com>
2020-12-27 10:02:43 -0500

    fixed img alt

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,14 @@
-import { Link } from 'react-router-dom';
 import LinkedInIcon from '../media/LinkedIn-icon.png';
 import GitHubIcon from '../media/GitHub-icon.png';
 
 const Footer = () => {
   return (
     <div className="footer-container">
-      <a href="https://www.linkedin.com/in/shelbygottschall/" target="_blank">
-        <img className="footer-icon" src={LinkedInIcon} />
+      <a href="https://www.linkedin.com/in/shelbygottschall/" target="_blank" rel="noreferrer">
+        <img className="footer-icon" src={LinkedInIcon} alt="LinkedIn icon"/>
       </a>
-      <a href="https://github.com/Shelby-Jean" target="_blank">
-      <img className="footer-icon" src={GitHubIcon} />
+      <a href="https://github.com/Shelby-Jean" target="_blank" rel="noreferrer">
+      <img className="footer-icon" src={GitHubIcon} alt="GitHub icon"/>
       </a>
     </div>
   )
