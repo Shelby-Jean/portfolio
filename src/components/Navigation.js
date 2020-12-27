2707feda9b39005e186146cab791b94b54301dc7
Shelby-Jean <shelby.gottschall@gmail.com>
2020-12-27 09:59:20 -0500

    updated nav bar and footer icons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,10 +2,10 @@ import { Link } from'react-router-dom';
 
 const Navigation = () => {
   return (
-    <div>
-      <h3 className="nav-container"> <Link to="/" className="nav-link">Home</Link> </h3>
-      <h3 className="nav-container"> <Link to="/about" className="nav-link">About</Link> </h3>
-      <h3 className="nav-container"> <Link to="/work" className="nav-link">Work</Link> </h3>
+    <div className="nav-container">
+      <Link to="/" className="nav-link"> <h3>Home</h3> </Link>
+      <Link to="/about" className="nav-link"> <h3>About</h3> </Link>
+      <Link to="/work" className="nav-link"> <h3>Work</h3> </Link>
     </div>
   )
 }
