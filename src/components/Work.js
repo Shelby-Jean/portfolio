3a00ebad26c1e6798bebbff8c1626dd51a579fd7
Shelby-Jean <shelby.gottschall@gmail.com>
2020-12-29 19:00:05 -0500

    tweaked header and nav font size

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navigation from './Navigation';
+import Header from './Header';
 
 class Work extends React.Component {
   render() {
     return (
       <div id="bg-img">
+        <Header />
         <Navigation />
       </div>
     )
